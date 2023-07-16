ac4f6ea79f60e71d779b71689a74ad02316e19e9
Richpong212 <richpong123@gmail.com>
2023-07-17 00:29:21 +0200

    updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,7 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import rental2 from "../../Assets/Projects/rental2.png";
 import rental from "../../Assets/Projects/rental.png";
+import perntodo from "../../Assets/Projects/pern-todo.png";
 
 function Projects() {
   return (
@@ -36,6 +37,15 @@ Tech: Nodejs,reactjs,express,MongoDB"
               title="RealDeal"
               description="The Real Estate Project is build using MERN Stack, The website has a responsive design that works on desktop, tablet, and mobile devices. The app  includes features such as user authentication, property search, property listings, property details, contact agent, and an admin panel. By leveraging the strengths of MongoDB, Express.js, React.js, and Node.js. I created a responsive and user-friendly experience for users looking to browse and search for properties online."
               ghLink="https://github.com/Richpong212/RentalService-Customer"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={perntodo}
+              isBlog={false}
+              title="Simple Pern Todo App"
+              description="The PERN Todo App is a sleek and efficient application built with PostgreSQL, Express, React, and Node.js. It allows users to manage their tasks easily with features like create, read, update, and delete functionalities. This showcases my expertise in full-stack web development and demonstrates my ability to leverage the power of the PERN stack for building practical and user-friendly applications."
+              ghLink="https://github.com/Richpong212/PERN-STACK-TODO"
             />
           </Col>
         </Row>
